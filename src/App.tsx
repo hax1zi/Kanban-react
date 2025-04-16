eeddaa71e5c2dc897edcb820ca25a018105f47d3
hax1zi <davidfontes303@gmail.com>
2025-04-16 12:56:46 -0300

    feat:preparing development environment

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className="w-screen h-screen bg-background dark:bg-background-dark">
+      <h1>Hello</h1>
+    </main>
+  );
+}
+
+export default App;
