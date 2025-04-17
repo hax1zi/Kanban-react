404f6a6ade2aee837b95f67c1fb89400c19deb99
hax1zi <davidfontes303@gmail.com>
2025-04-17 18:25:44 -0300

    feat:first card movements

--- src/components/cards/areas/MainArea.tsx
+++ src/components/cards/areas/MainArea.tsx
@@ -5,25 +5,44 @@ import AddArea from "./AddArea";
 
 export default function MainArea() {
   const {data} = useCardsData()
+  const {moveCard} = useCardsData()
+
   return (
     <div className="h-full flex gap-5 flex-row m-4 rounded-lg shadow-lg ">
       <AddArea width="350px">
-        <div className="h-full w-full box-border flex flex-col shadow-md">
+        <div className="h-full w-full box-border flex flex-col shadow-md" onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => {
+            const droppedData = e.dataTransfer.getData("application/json")
+
+            const card = JSON.parse(droppedData)
+            moveCard(card, "backlog")
+          }}>
           <div className="bg-backlog dark:bg-backlog-dark h-14 p-4 rounded-t-lg">
             <h2 className="font-medium">Backlog</h2>
           </div>
           <CreateCard/>
           {data.backlog.map((card, index) => (
-            <Card key={index} className=" dark:bg-backlog-dark bg-backlog" title={card.title}/>
+            <Card status={card.status} key={index} className=" dark:bg-backlog-dark bg-backlog" priority={card.priority} title={card.title}/>
           ))}
         </div>
       </AddArea>
+      
       <AddArea width="99%" >
         <div className="h-full w-full flex">
-          <div className="h-full w-[33.333%] shadow-md">
+          <div className="h-full w-[33.333%] shadow-md" onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => {
+            const droppedData = e.dataTransfer.getData("application/json")
+
+            const card = JSON.parse(droppedData)
+            moveCard(card, "todo")
+          }}
+          >
             <div className="bg-todo dark:bg-todo-dark h-14 p-4 rounded-tl-lg">
               <h2 className="font-medium">Para fazer</h2>
             </div>
+            {data.todo.map((card, index) => (
+            <Card status={card.status} key={index} className=" dark:bg-backlog-dark mt-2 bg-backlog" priority={card.priority} title={card.title}/>
+          ))}
           </div>
           <div className="h-full w-[33.333%] shadow-md">
             <div className="bg-inProgress dark:bg-inProgress-dark h-14 p-4">
--- src/store/useCardsData.ts
+++ src/store/useCardsData.ts
@@ -10,12 +10,13 @@ type CardsData = {
   backlog: Card[]
   todo: Card[]
   inProgress: Card[]
-  Done: Card[]
+  done: Card[]
 }
 
 type store = {
   data: CardsData
   pushBacklog: (card: Card) => void
+  moveCard: (card: Card, targetStatus: string) => void
 }
 
 const useCardsData = create<store>((set) => ({
@@ -23,7 +24,7 @@ const useCardsData = create<store>((set) => ({
     backlog: [],
     todo: [],
     inProgress: [],
-    Done: [],
+    done: [],
   },
   pushBacklog: (cards) => set((state) => ({
     data: {
@@ -31,9 +32,22 @@ const useCardsData = create<store>((set) => ({
       backlog: [
         ...state.data.backlog,
         ...(Array.isArray(cards) ? cards : [cards])
-      ]
+      ],
     }
-  }))
+  })),
+  moveCard: (card: Card, targetStatus: string) => set((state) => {
+    
+    const newData: CardsData = {
+      backlog: state.data.backlog.filter((c) => c.title !== card.title),
+      todo: state.data.todo.filter((c) => c.title !== card.title),
+      inProgress: state.data.inProgress.filter((c) => c.title !== card.title),
+      done: state.data.done.filter((c) => c.title !== card.title),
+    }
+
+    newData[targetStatus as keyof CardsData].push({ ...card, status: targetStatus })
+
+      return { data: newData }
+  })
 }))
 
 export default useCardsData
